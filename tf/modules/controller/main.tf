91688e57094f85b7623e36bf623503ffeddba378
AntreasAntoniou <a.antoniou@ed.ac.uk>
2022-01-19 03:09:37 +0000

    Clean and add improvements for pre-emptible usage.

--- tf/modules/controller/main.tf
+++ tf/modules/controller/main.tf
@@ -52,7 +52,7 @@ resource "google_compute_disk" "secondary" {
   zone = var.zone
 }
 
-data "google_compute_default_service_account" "default" {}
+data "google_compute_default_service_account" "tali-multi-modal" {}
 
 resource "google_compute_instance" "controller_node" {
   count = var.instance_template == null ? 1 : 0
